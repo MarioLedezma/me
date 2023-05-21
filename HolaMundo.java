2ce1feae66e3f0324eb75f6b0cda044ed0c95fd6
Mario Ledezma <133705185+MarioLedezma@users.noreply.github.com>
2023-05-21 15:59:21 -0600

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,1 +1,13 @@
+package pkg1.holamundo;
 
+
+public class HolaMundo {
+
+    
+    public static void main(String[] args) {
+       
+        System.out.println("Hola Mundo");
+        
+    }
+    
+}
